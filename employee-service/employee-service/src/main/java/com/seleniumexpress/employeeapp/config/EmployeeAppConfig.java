7a26515adf2c9e3a303c26d234017255f69bf63a
mehdi47 <jackie_3_22@hotmail.com>
2023-10-11 01:29:50 +0100

    updated employee app with webclient instead of resttemplate

--- employee-service/employee-service/src/main/java/com/seleniumexpress/employeeapp/config/EmployeeAppConfig.java
+++ employee-service/employee-service/src/main/java/com/seleniumexpress/employeeapp/config/EmployeeAppConfig.java
@@ -1,18 +1,29 @@
 package com.seleniumexpress.employeeapp.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class EmployeeAppConfig {
+
+    @Value("${addressservice.base.url}") // if you use the addressbaseurl in the constructor it will be null, you need to add it to the constructor like this
+    private String adressbaseURL; // @Value("${addressservice.base.url}") String adressbaseURL
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
 
-//    @Bean
-//    public RestTemplate restTemplate(){ return new RestTemplate(); };
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient
+                .builder()
+                .baseUrl(adressbaseURL)
+                .build();
+    }
 }
--- employee-service/employee-service/src/main/java/com/seleniumexpress/employeeapp/service/EmployeeService.java
+++ employee-service/employee-service/src/main/java/com/seleniumexpress/employeeapp/service/EmployeeService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class EmployeeService {
@@ -18,17 +19,17 @@ public class EmployeeService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private WebClient webClient;
+
 //    @Autowired
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
 
-//    @Value("${addressservice.base.url}") // if you use the addressbaseurl in the constructor it will be null, you need to add it to the constructor like this
-//    private String adressbaseURL; // @Value("${addressservice.base.url}") String adressbaseURL
-
-    public EmployeeService(@Value("${addressservice.base.url}") String adressbaseURL, RestTemplateBuilder builder){
-        this.restTemplate = builder
-                            .rootUri(adressbaseURL)
-                            .build();
-    }
+//    public EmployeeService(@Value("${addressservice.base.url}") String adressbaseURL, RestTemplateBuilder builder){
+//        this.restTemplate = builder
+//                            .rootUri(adressbaseURL)
+//                            .build();
+//    }
 
     public EmployeeResponse getEmployeeById(int id){
 
@@ -36,9 +37,15 @@ public class EmployeeService {
 
         var employeeResponse = modelMapper.map(employee, EmployeeResponse.class);
 
-        var addressResponse = restTemplate.getForObject("/address/{id}", AddressResponse.class, id);
+        var addressResponse = webClient.get().uri("/address/"+id).retrieve()
+                .bodyToMono(AddressResponse.class)
+                .block();
         employeeResponse.setAddressResponse(addressResponse);
 
         return employeeResponse;
     }
+
+//    private AddressResponse callingAddressServiceUsingRestTemplate(int id) {
+//       return restTemplate.getForObject("/address/{id}", AddressResponse.class, id);
+//    }
 }
